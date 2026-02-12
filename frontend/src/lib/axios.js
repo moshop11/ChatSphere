1e076bb06e9209701aa815bf3baabff6ba2b0337
moshop11 <shantanumohod11@gmail.com>
2026-02-12 23:42:11 +0530

    fix backend url for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://chatsphere-1-5xw8.onrender.com/api",
   withCredentials: true,
 });
+
